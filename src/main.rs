4b63ddbad09f9f0591ad2686ae7abc8060929c4e
kevin <kevin91cooland@gmail.com>
2023-11-14 00:39:35 +0800

    Mvp 0.0.1

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use std::io::{self};
-use sysy_alpha::{
-    lexer::{self, tokenize},
-    utils::print_tokens,
-};
+use std::path::Path;
+use sysy_alpha::{lexer::tokenize, utils::print_tokens};
 
 fn main() {
     println!("请输入文件路径:");
@@ -14,4 +12,16 @@ fn main() {
     println!("您输入的文件路径为: {}", file_path);
 
     let tokens = tokenize(file_path);
+
+    println!("请输入保存路径:");
+    let mut save_path = String::new();
+    io::stdin()
+        .read_line(&mut save_path)
+        .expect("Failed to read line");
+
+    println!("您输入的保存路径为: {}", save_path);
+
+    print_tokens(&tokens, &Path::new(&save_path));
+
+    println!()
 }
