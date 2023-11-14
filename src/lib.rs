27204288593c8834e4da588b551d043ce6f0c579
kevin <kevin91cooland@gmail.com>
2023-11-14 21:05:02 +0800

    Add BasicType and NodeType

--- src/lib.rs
+++ src/lib.rs
@@ -57,3 +57,48 @@ pub enum TokenType {
     LeftBrace,
     RightBrace,
 }
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum BasicType {
+    Nil,
+    Int,
+    Float,
+    Const, //这里的Const现在约定是整形常数.
+    Void,
+    IntArray(Vec<usize>),
+    FloatArray(Vec<usize>),
+    ConstArray(Vec<usize>), //约定是整形常数数组.
+    Func(Box<BasicType>),   //return type
+}
+
+pub enum NodeType {
+    /*
+        以下每一个枚举成员都可能是Ast中的一个Node所属的类型之一
+        根据Rust枚举类型可以带值的特性,可以把变元对应的产生式放入其中.
+    */
+
+    /* 常变量声明-获取类 */
+    Decl,
+    DeclStmt,
+    InitList,
+    Assign,
+    BinOp,
+    Aceess,
+
+    /* 函数类 */
+    FuncDef,
+    Block,
+    Return,
+    Call,
+
+    /* 结构-循环类 */
+    If,
+    While,
+    Conitnue,
+    Break,
+
+    /* 结点值类 */
+    Nil,
+    Number(i32),
+    FloatNumber(f32),
+}
